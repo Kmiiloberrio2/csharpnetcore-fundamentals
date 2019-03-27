7c6652137f3119114a007c1572181fbd5161f06b
Camilo.Berrio <kmiiloberrio@gmail.com>
2019-03-27 16:01:57 -0500

    Clases y objetos, example.

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    internal class Escuela
+    {
+        public int AñoFundacion;
+        public string Ceo;
+        public string Direccion;
+        public string Nombre;
+
+        public void Timbrar()
+        {
+            Console.Beep(987, 1000); //Si
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Escuela miEscuela = new Escuela();
+
+            miEscuela.Nombre = "Platzi Academy";
+            miEscuela.Direccion = "Cr 9 calle 72";
+            miEscuela.AñoFundacion = 2012;
+            miEscuela.Ceo = "Fredy Vega";
+
+            Console.WriteLine("TIMBRAR");
+
+            miEscuela.Timbrar();
+        }
+    }
+}
